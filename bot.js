1dd77318e848dfcd92117ea0b15ea9430c42e103
YASSIN974 <44032576+YASSIN974@users.noreply.github.com>
2018-11-04 21:17:16 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ client.on('ready', function(){
  game: { 
     type: 1,
      url: 'https://www.twitch.tv/skwadraa',
-    name: 'CUTE',
+    name: 'The response is strong',
     application_id: '477187715658547201',
      assets: {
          large_image:   `${s[Math.floor(Math.random() * s.length)]}`,
@@ -20,4 +20,4 @@ client.on('ready', function(){
     });
     }, 5000);
 });
-client.login(process.env.BOT_TOKEN);a
+client.login(process.env.BOT_TOKEN);
